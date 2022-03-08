e6a9def2669ba0c1be6ad9f90296143842b76570
sranjeet612 <sranjeet612@gmail.com>
2022-03-08 08:21:56 +0530

    added login component

--- AngularAPPBasic/src/app/app.module.ts
+++ AngularAPPBasic/src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { UserAuthComponent } from './user-auth/user-auth.component';
+import { UserLoginComponent } from './User-Auth/user-login/user-login.component';
+import { UserRegisterComponent } from './User-Auth/user-register/user-register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    UserAuthComponent,
+    UserLoginComponent,
+    UserRegisterComponent
   ],
   imports: [
     BrowserModule
