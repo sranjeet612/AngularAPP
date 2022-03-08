0753dfd2a01606cf8bd4fb351a9c19862ca4fbf0
sranjeet612 <sranjeet612@gmail.com>
2022-03-08 08:25:39 +0530

    modified Appmodule.ts file

--- AngularAPPBasic/src/app/app.module.ts
+++ AngularAPPBasic/src/app/app.module.ts
@@ -3,17 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { UserAuthComponent } from './user-auth/user-auth.component';
-import { UserLoginComponent } from './User-Auth/user-login/user-login.component';
-import { UserRegisterComponent } from './User-Auth/user-register/user-register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    UserAuthComponent,
-    UserLoginComponent,
-    UserRegisterComponent
+    LoginComponent
   ],
   imports: [
     BrowserModule
