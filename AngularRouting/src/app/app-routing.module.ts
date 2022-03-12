8cfc7c5de53c9d98665e0c6a7027012e46f3c0dd
sranjeet612 <sranjeet612@gmail.com>
2022-03-12 18:07:27 +0530

    Routing Module Demo

--- AngularRouting/src/app/app-routing.module.ts
+++ AngularRouting/src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AboutComponent } from './about/about.component';
+import { HomeComponent } from './home/home.component';
+import { UserLoginComponent } from './user-login/user-login.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"", component:HomeComponent},
+  {path:"about",component:AboutComponent},
+  {path:"user-login",component:UserLoginComponent},
+  {path:"user-register",component:UserRegisterComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- AngularRouting/src/app/app.module.ts
+++ AngularRouting/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { UserRegisterComponent } from './user-register/user-register.component';
+import { UserLoginComponent } from './user-login/user-login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    UserRegisterComponent,
+    UserLoginComponent
   ],
   imports: [
     BrowserModule,
